14a957b9be4b8db6c73f666dc063408017cd6850
Parwiz Rahimi <farihajan@Parwizs-MacBook-Pro.local>
2017-10-21 12:53:32 -0400

    added file

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log("Keys loaded");
+
+
+var twitterKeys = {
+  consumer_key: 'cDJrjJtCPgjwRgYQaQWs8J7Ar',
+  consumer_secret: '1Fuwu5QSrFRmdLv6SMI5zMvuvxAI9c9DRX9x06DBW6GdfvrHPp',
+  access_token_key: '2705669371-zSYZV09rBSQ8alxm8z97N5EVql4eZympf3GBdEP',
+  access_token_secret: 'jmpqN8KkC05OI2v3AL0ZK1zYFps8RpG3dbDMKbBGpUhnX'
+};
+var spotifyKeys = {
+  id: '75a2456e0e504b0894cb74899305c9ea',
+  secret: '860415517c0a44df92f341bdb9e8c226'
+};
+
+module.exports = {twitterKeys:twitterKeys, spotify:spotifyKeys};
